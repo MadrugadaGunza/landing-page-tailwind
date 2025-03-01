7ac22c63e0ede798e0c0a85681d7d413bd964db0
Madrugada de Carvalho Calunga Gunza <madrugadaoca@gmail.com>
2025-03-01 02:38:56 +0100

    feat: add tailwind and clean project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-red-600 p-80'>
+      <h1 className='text-sky-500'>teste</h1>
+      <h1 className='bg-red-300'>
+        teste 01
+      </h1>
+    </div>
+  )
+}
+
+export default App
